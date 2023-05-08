e2075976d896c64f52472e98d5a20e5c90352a58
js012108 <js012108@gmail.com>
2023-05-08 06:55:50 -0500

    Ejemplo extendido

--- client.py
+++ client.py
@@ -2,11 +2,44 @@ import socket
 import json
 
 HOST, PORT = "localhost", 9527
-data = {
-"name": "Hola, Soy Tom.",
-"age": 10,
-"info": "Muestra de información."
-}
+data = {}
+data['Libros'] = []
+
+data['Libros'].append({
+    'Titulo': 'El lenguaje de programación Python',
+    'Autores': 'Challenger-Pérez, I., Díaz-Ricardo, Y., & Becerra-García',
+    'Tema': 'Programación',
+    'Editorial': 'Ciencias Holguín',
+    'Año': 2014})
+
+data['Libros'].append({
+    'Titulo': 'Python para todos',
+    'Autores': 'González Duque',
+    'Tema': 'Programación',
+    'Editorial': 'NA',
+    'Año':2011})
+
+data['Libros'].append({
+    'Titulo': 'Los elementos del periodismo',
+    'Autores': 'Kovach, B., & Rosenstiel, T',
+    'Tema': 'Periodimos',
+    'Editorial':'Aguilar',
+    'Año': 2012})
+
+data['Libros'].append({
+    'Titulo': 'Los dueños del periodismo ',
+    'Autores': 'Reig, R. ',
+    'Tema': 'Periodismo',
+    'Editorial': 'Editorial Gedisa',
+    'Año':2011})
+
+data['Libros'].append({
+    'Titulo': 'Actividades físicas y deportes adaptados para personas con discapacidad',
+    'Autores': 'Rivas, D. S',
+    'Tema': 'Deportes',
+    'Editorial': 'Paidotribo',
+    'Año':2013}) 
+
 
 # Crear un socket(SOCK_STREAM means a TCP socket)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
